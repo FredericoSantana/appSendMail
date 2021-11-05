c4df0485bf5aba0e075c83b745fffdd20b8734a7
FredericoSantana <fredericosantana@hotmail.com.br>
2021-11-05 10:06:21 -0300

    Melhorando o feedback visual

--- processa_envio.php
+++ processa_envio.php
@@ -13,6 +13,10 @@ class Mensagem
   private $para = null;
   private $assunto = null;
   private $mensagem = null;
+  public $status = [
+    'codigo_status' => null,
+    'descricao_status' => ''
+  ];
 
   public function __get($atributo)
   {
@@ -24,7 +28,7 @@ class Mensagem
     $this->$atributo = $valor;
   }
 
-  public function mensagemValida()
+  public function mensagemValida(): bool
   {
     if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
       return false;
@@ -44,7 +48,7 @@ $mensagem->__set('mensagem', $_POST['mensagem']);
 
 if (!$mensagem->mensagemValida()) {
   echo 'Mensagem não é válida';
-  die();
+  header('Location: index.php');
 }
 
 $mail = new PHPMailer(true);
@@ -52,14 +56,14 @@ $mail = new PHPMailer(true);
 try {
   //Server settings
 //  $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-  $mail->SMTPDebug = 2;                      //Enable verbose debug output
+  $mail->SMTPDebug = false;                      //Enable verbose debug output
   $mail->isSMTP();                                            //Send using SMTP
-  $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-  $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-  $mail->Username   = 'fredericosantana11@gmail.com';                     //SMTP username
-  $mail->Password   = 'engels967947';                               //SMTP password
+  $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+  $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+  $mail->Username = 'fredericosantana11@gmail.com';                     //SMTP username
+  $mail->Password = 'engels967947';                               //SMTP password
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-  $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+  $mail->Port = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
   //Recipients
   $mail->setFrom('fredericosantana11@gmail.com', 'Frederico Santana');
@@ -74,13 +78,54 @@ try {
 
   //Content
   $mail->isHTML(true);                                  //Set email format to HTML
-  $mail->Subject = $mensagem->__get('assunto');
-  $mail->Body    = $mensagem->__get('mensagem');
+  $mail->Subject = utf8_decode($mensagem->__get('assunto'));
+  $mail->Body = utf8_decode($mensagem->__get('mensagem'));
   $mail->AltBody = 'É necessário ter um client que suporte HTML.';
 
   $mail->send();
-  echo 'E-mail enviado com sucesso.';
-} catch (Exception $e) {
-  echo "Não foi possível enviar o e-mail. Mailer Error: {$mail->ErrorInfo}";
-}
 
+  $mensagem->status['codigo_status'] = 1;
+  $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso.';
+
+} catch (Exception $e) {
+  $mensagem->status['codigo_status'] = 2;
+  $mensagem->status['descricao_status'] = 'Não foi possível enviar o e-mail. Mailer Error: ' . $mail->ErrorInfo;
+}
+?>
+
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>App Mail Send</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+    <div class="py-3 text-center">
+      <img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+      <h2>Send Mail</h2>
+      <p class="lead">Seu app de envio de e-mails particular!</p>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <?php if ($mensagem->status['codigo_status'] == 1) { ?>
+          <div class="container">
+            <h1 class="display-4 text-success">Sucesso</h1>
+            <p><?= $mensagem->status['descricao_status'] ?></p>
+            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+          </div>
+        <?php } ?>
+
+        <?php if ($mensagem->status['codigo_status'] == 2) { ?>
+          <div class="container">
+            <h1 class="display-4 text-danger">Ops!</h1>
+            <p><?= $mensagem->status['descricao_status'] ?></p>
+            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
